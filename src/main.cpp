5d232c525a009e104c9a99b7a819a62f618efc64
blowfishfugu <coolm12345@hotmail.de>
2023-10-29 16:22:25 +0100

    writing a ppm in ascii
    points of concern:
    windows linefeed couts to crlf
    cout of int8_t would write binary bytevalue without transformation to
    ascii

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,60 @@
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <array>
+#include <string_view>
+using byte = int;
+
+struct ppm_pixel {
+	byte r{};
+	byte g{};
+	byte b{};
+};
+
+std::ostream& operator<<(std::ostream& os, ppm_pixel const& p)
+{
+	os << p.r << ' ' << p.g << ' ' << p.b << '\n';
+	return os;
+}
+
+struct ppm_head {
+	const std::string_view magic{ "P3\n" };
+	const std::string_view color_max{ "\n255\n"};
+	int image_width{};
+	int image_height{};
+	explicit ppm_head(int width,int height) 
+		: image_width{width},image_height{height}
+	{}
+
+};
+
+std::ostream& operator<<(std::ostream& os, ppm_head const& head)
+{
+	os	<< head.magic 
+		<< head.image_width << ' ' << head.image_height 
+		<< head.color_max;
+	return os;
+}
+
 int main(int argc, char** argv)
 {
+	ppm_head head{ 256,256 };
+	std::cout << head;
+	for (int y = 0; y < head.image_height; ++y)
+	{
+		for (int x = 0; x < head.image_width; ++x)
+		{
+			double ratioR = double(x) / (head.image_width - 1);
+			double ratioG = double(y) / (head.image_height - 1);
+			double b = 0.0;
+
+			ppm_pixel pxl{
+				.r = static_cast<byte>(ratioR * 255.999),
+				.g = static_cast<byte>(ratioG * 255.999),
+				.b = static_cast<byte>(b * 255.999)
+			};
+			std::cout << pxl;
+		}
+	}
 	return 0;
 }
